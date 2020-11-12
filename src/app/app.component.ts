f31944317699d87727a69c3448db8316c220a8e3
merlinpratheesh <merlinpratheesh@gmail.com>
2020-11-12 18:11:48 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { BottomSheetExample } from './to-bottomsheet/bottom-sheet-example';
 
 @Component({
+
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
+
+  constructor(private bottomSheet: MatBottomSheet) {
+  }
+  showBottomSheet() {
+    this.bottomSheet.open(BottomSheetExample);
+  }
 }
---
+++ src/app/to-bottomsheet/bottom-sheet-example.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-bottom-sheet',
+  templateUrl: './bottom-sheet-example.html',
+  styleUrls: [],
+})
+export class BottomSheetExample {}
